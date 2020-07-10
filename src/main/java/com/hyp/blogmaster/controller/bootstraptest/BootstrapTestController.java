7f1a12b5fe418e898ee238714a5e078a57d08316
heyapei <15518901416@163.com>
2020-07-10 20:17:37 +0800

    添加bootstrap测试页面

---
+++ src/main/java/com/hyp/blogmaster/controller/bootstraptest/BootstrapTestController.java
@@ -0,0 +1,32 @@
+package com.hyp.blogmaster.controller.bootstraptest;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author 何亚培
+ * @Version V1.0
+ * @Date 2020/5/31 10:48
+ * @Description: TODO
+ */
+@Controller
+@RequestMapping("/bootstrap/test")
+@Slf4j
+@Api(value = "bootstrap测试用")
+public class BootstrapTestController {
+
+    /**
+     * 进入首页
+     *
+     * @return
+     */
+    @GetMapping("test1")
+    public String goIndex() {
+        return "/bootstraptest/test1";
+    }
+
+
+}

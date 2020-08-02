8e91bc8bce35aaef6582cb1fea3b4bfac08554fb
heyapei <15518901416@163.com>
2020-08-02 15:14:37 +0800

    漏了

---
+++ src/main/java/com/hyp/blogmaster/shiro/mapper/UserSupplyInfoMapper.java
@@ -0,0 +1,10 @@
+package com.hyp.blogmaster.shiro.mapper;
+
+
+import com.hyp.blogmaster.shiro.pojo.modal.UserSupplyInfo;
+import com.hyp.blogmaster.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSupplyInfoMapper extends MyMapper<UserSupplyInfo> {
+}
